3c8cd230dd2a8078ac9843344ff91c0869980083
Chimamanda16 <chimamandaunachukwu@gmail.com>
2025-02-16 17:23:43 +0100

    Update route

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://chat-app-1ta0.onrender.com/login/api',
+    baseURL: 'https://chat-app-1ta0.onrender.com/api',
     withCredentials: true
 })
